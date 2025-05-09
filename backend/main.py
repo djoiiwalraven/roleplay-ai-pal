7f8bd3a97f3dfa543fcce1dc987af8c12d7e91d6
ddwalraven <dd.walraven@gmail.com>
2025-05-09 17:06:01 +0200

    allow any origin for now

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,       # or ["*"] while debugging
+    allow_origins=["*"],       # or ["*"] while debugging
     allow_credentials=True,
     allow_methods=["*"],         # "OPTIONS" will be handled automatically
     allow_headers=["*"],         # needed for JSON
